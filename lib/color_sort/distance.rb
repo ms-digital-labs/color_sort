1a457e143edb66124b8eb2188af0b29695845822
Pip Taylor <pip@evilgeek.co.uk>
2014-04-28 15:54:37 +0100

    Make ciede2000 a class method.
    And introduce rspec-radar so I don't stub methods that don't exist any more.

--- lib/color_sort/distance.rb
+++ lib/color_sort/distance.rb
@@ -2,14 +2,14 @@ require "colorscore"
 
 module ColorSort
   module Distance
-    def ciede2000(lab_color_a, lab_color_b)
+    def self.ciede2000(lab_color_a, lab_color_b)
       cache_key = [lab_color_a, lab_color_b].sort
       ciede2000_cache[cache_key] ||=
         Colorscore::Metrics.delta_e_cie_2000(*lab_color_a, *lab_color_b)
     end
 
   private
-    def ciede2000_cache
+    def self.ciede2000_cache
       @ciede2000_cache ||= {}
     end
   end
--- spec/lib/distance_spec.rb
+++ spec/lib/distance_spec.rb
@@ -2,15 +2,13 @@ require "spec_helper"
 
 describe ColorSort::Distance do
   describe "#ciede2000" do
-    include ColorSort::Distance
-
     let(:distance) { double(:distance) }
 
     it "splats the lab colors and passes them to Colorscore" do
       expect(Colorscore::Metrics).to receive(:delta_e_cie_2000)
                                       .with(1, 2, 3, 4, 5, 6)
 
-      ciede2000([1, 2, 3], [4, 5, 6])
+      ColorSort::Distance.ciede2000([1, 2, 3], [4, 5, 6])
     end
 
     it "only calls in to Colorscore once for each pair of colors" do
@@ -19,8 +17,8 @@ describe ColorSort::Distance do
                                       .once
                                       .and_return(distance)
 
-      expect(ciede2000([1, 2, 3], [4, 5, 6])).to eq(distance)
-      expect(ciede2000([4, 5, 6], [1, 2, 3])).to eq(distance)
+      expect(ColorSort::Distance.ciede2000([1, 2, 3], [4, 5, 6])).to eq(distance)
+      expect(ColorSort::Distance.ciede2000([4, 5, 6], [1, 2, 3])).to eq(distance)
     end
   end
 end
