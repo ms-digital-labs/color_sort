4d597aa6ccece3c58b4d590185886a18116becea
Pip Taylor <pip@evilgeek.co.uk>
2014-04-28 11:09:22 +0100

    Remove dummy spec.

--- spec/color_sort_spec.rb
+++ spec/color_sort_spec.rb
@@ -4,8 +4,4 @@ describe ColorSort do
   it 'should have a version number' do
     ColorSort::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should eq(true)
-  end
 end
